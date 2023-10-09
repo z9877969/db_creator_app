cb95c38e502fe8c117bc3a304a4ea3464782ff72
z9877969 <z9877969@ukr.net>
2023-10-09 11:31:01 +0300

    adds PATCH /users/info

--- models/user/userValidationSchemas.js
+++ models/user/userValidationSchemas.js
@@ -2,6 +2,7 @@ const Joi = require("joi");
 const mongoose = require("mongoose");
 const {
   userSchema: constants,
+  transactionSchema: { TRANSACTIONS_CURRENCY },
   regex: { EMAIL_REGEX },
 } = require("../../constants");
 
@@ -46,16 +47,10 @@ const refreshTokensValidationSchema = Joi.object({
 });
 
 const updateUserInfoValidationSchema = Joi.object({
-  email: Joi.string().pattern(EMAIL_REGEX),
-  password: Joi.string()
-    .min(constants.PASSWORD_LENGTH.MIN)
-    .max(constants.PASSWORD_LENGTH.MAX),
-  oldPassword: Joi.string()
-    .min(constants.PASSWORD_LENGTH.MIN)
-    .max(constants.PASSWORD_LENGTH.MAX),
   name: Joi.string()
     .min(constants.USER_NAME_LENGTH.MIN)
     .max(constants.USER_NAME_LENGTH.MAX),
+  currency: Joi.string().valid(...Object.values(TRANSACTIONS_CURRENCY)),
 });
 
 module.exports = {
